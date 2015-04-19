915923e935505f457078fac9a4d62715babf7e19
dvchinh <npe.etc@gmail.com>
2015-04-19 16:44:52 +0700

    N/A

---
+++ DataModel/Models/Products/Price.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using ServiceStack.OrmLite;
+using ServiceStack.DataAnnotations;
+
+namespace PhotoBookmart.DataLayer.Models.Products
+{
+    public enum Enum_Price_MasterType
+    {
+        ProductOption,
+        ProductShippingPrice,
+        Product,
+    }
+
+    /// <summary>
+    /// Remember to delete the Price when you delete object for we dont know exactly which object connect to the price
+    /// </summary>
+    [Schema("Products")]
+    public partial class Price : ModelBase
+    {
+        ///// <summary>
+        ///// Price we display according to the country
+        ///// </summary>
+        //public double DisplayPrice { get; set; }
+        ///// <summary>
+        ///// Price we use to calculate, in RM
+        ///// </summary>
+        //public double RealPrice { get; set; }
+
+        /// <summary>
+        /// Price in this currency
+        /// </summary>
+        public double Value { get; set; }
+
+        /// <summary>
+        /// Vietnam, Malaysia,...
+        /// https://developer.paypal.com/docs/classic/api/country_codes/
+        /// </summary>
+        public string CountryCode { get; set; }
+
+        /// <summary>
+        /// VND, đ, RM, USD, to show to customer
+        /// </summary>
+        public string CurrencyCode { get; set; }
+        ///// <summary>
+        ///// This is paypal code
+        ///// https://developer.paypal.com/docs/classic/api/currency_codes/
+        ///// </summary>
+        //public string CurrencyCode_ThereLetters { get; set; }
+
+        /// <summary>
+        /// Id of the object which hold the link to this price
+        /// </summary>
+        public long MasterId { get; set; }
+
+        /// <summary>
+        /// Type of the object hold this price
+        /// </summary>
+        public Enum_Price_MasterType MasterType { get; set; }
+
+        [Ignore]
+        public string Master_Name { get; set; }
+
+        public Price()
+        {
+
+        }
+    }
+}
---
+++ DataModel/Models/Products/Product.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using ServiceStack.OrmLite;
+using ServiceStack.DataAnnotations;
+
+namespace PhotoBookmart.DataLayer.Models.Products
+{
+    [Alias("Products")]
+    [Schema("Products")]
+    public partial class Product : ModelBase
+    {
+        public bool Status { get; set; }
+
+        public string Name { get; set; }
+
+        public List<string> Tag { get; set; }
+
+        public string ShortDescription { get; set; }
+
+        public string Description { get; set; }
+
+        public bool PriceDontShow { get; set; }
+
+        public bool PriceCallForPrice { get; set; }
+
+        public bool isFreeShip { get; set; }
+
+        public bool ShowOnHomepage { get; set; }
+
+        public string SeoName { get; set; }
+
+        // thứ tự sắp xếp
+        public int Order { get; set; }
+
+        public string Size { get; set; }
+
+        public int Pages { get; set; }
+
+        public string Paper { get; set; }
+
+        public string Orientation { get; set; }
+
+        public long CatId { get; set; }
+        [Ignore]
+        public string Category_Name { get; set; }
+
+        /// <summary>
+        ///  This is the pID from the My Photo Creation. We follow them in order to capture the product 
+        /// </summary>
+        public int MyPhotoCreationId { get; set; }
+
+        /// <summary>
+        /// Weight of the product, in grams
+        /// </summary>
+        public double Weight { get; set; }
+
+        /// <summary>
+        /// If yes, then when customer submit order, they can select cover material
+        /// </summary>
+        public bool IsAllowCoverMaterialSelect { get; set; }
+
+        #region Front End use
+        /// <summary>
+        /// For Front End use only
+        /// Calculate the shipping cost on server before send it
+        /// </summary>
+        [Ignore]
+        public List<Price> ShippingPrice { get; set; }
+
+        /// <summary>
+        /// Product price, to be shown on order page
+        /// </summary>
+        [Ignore]
+        public double Price { get; set; }
+        #endregion
+
+        public Product()
+        {
+            Id = 0;
+
+            CreatedBy = 0;
+
+            CreatedOn = DateTime.Now;
+
+            Order = 0;
+
+            Tag = new List<string>();
+
+            SeoName = "";
+
+            Pages = 0;
+
+            ShippingPrice = new List<Price>();
+        }
+
+        public List<Product_Images> GetImages()
+        {
+            //var images = this.InternalService.Db.Where<Product_Images>(m => m.Status && m.ProductId == this.Id);
+            var images = Db.Where<Product_Images>(m => m.Status && m.ProductId == this.Id);
+            Db.Close();
+            return images;
+        }
+
+        public Price getPrice(Enum_Price_MasterType type, string countrycode)
+        {
+            if (string.IsNullOrEmpty(countrycode))
+            {
+                countrycode = "MY";
+            }
+
+            countrycode = countrycode.ToUpper().Trim();
+
+            var price = base.getPrice(this.Id, type, countrycode);
+            Price ret = null;
+            if (price.Count > 0)
+            {
+                if (string.IsNullOrEmpty(countrycode))
+                {
+                    // find RM, Malaysia
+                    ret = price.Where(x => x.CountryCode == "MY").FirstOrDefault();
+                    if (ret == null)
+                    {
+                        ret = price.FirstOrDefault();
+                    }
+                }
+                else
+                {
+                    ret = price.FirstOrDefault();
+                }
+            }
+            else
+            {
+                ret = new Price();
+            }
+            Db.Close();
+            return ret;
+        }
+    }
+}
